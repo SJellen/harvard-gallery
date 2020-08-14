9cdd9afc2effcea86e419c3f5e26de16b16ee515
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-08-14 16:00:54 -0400

    layout for gallery section and added Material ui

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react'
-
-
+import Button from '@material-ui/core/Button';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const apiKEY = process.env.REACT_APP_HARVARD_GALLERY
 
@@ -11,13 +11,17 @@ const apiKEY = process.env.REACT_APP_HARVARD_GALLERY
 function Gallery() {
 
         const [images, setImages] = useState([])
-        
+        const [isLoading, setIsLoading] = useState(true)
         const [page, setPage] = useState(0)
 
         const APIlink = `https://api.harvardartmuseums.org/image?apikey=${apiKEY}&page=${page}`
 
         useEffect(() => {
-                fetchImages()    
+                
+                
+                fetchImages()
+                setIsLoading(false)
+                
             }, [])
 
 
@@ -27,6 +31,7 @@ function Gallery() {
                 .then(res => res.json())
                 .then(data => setImages(data.records))
                 .catch(error => console.log(error))
+                
         }       
         
 
@@ -35,21 +40,97 @@ function Gallery() {
         
 
 
-        console.log(images[0])
+        console.log(page)
 
+        function decrementPage() {
+            if (page >= 1 ) {
+                setPage(page -1)
+                fetchImages()
+                 
+            }
+        }
 
+        function incrementPage() {
+            if (page <= 36999) {
+                setPage(page +1)
+                fetchImages()
+            }
+                
+                
+                
+                
+                 
+        }
                 
 
            
             return (
 
                 <div className="gallery">
-                    {images.map(image => (
+                <div className="button-box" id="button-box-top">
+                                {page === 0 ? 
+                                    <Button variant="contained" disabled onClick={decrementPage}>
+                                        {page === 0 ? '' : page === 1 ? page  : page}
+                                </Button> :
+                                <Button variant="contained" color="primary" onClick={decrementPage} >
+                                        {page === 0 ? '' : page === 1 ? page  : page}
+                                </Button>
+                                }
+                                
+                                <span>Page: {page + 1}/ 37000</span>
+
+                                {page >= 36999 ? 
+                                    <Button variant="contained" disabled onClick={incrementPage}>
+                                        {''}
+                                </Button> : 
+                                
+                                <Button variant="contained" color="primary" onClick={incrementPage}>
+                                        {page === 37001 ? '' : page === 37000 ? page + 1 : page + 2}
+                                </Button>
+                                }
+                                
+                
+                                </div>
+
+                <div className="image-box">
+                        {!isLoading ? images.map(image => (
                         <div key={image.fileid}>
                             <img src={image.baseimageurl} alt="art piece" className="gallery-image"/>
-                        </div>
+                        </div>   
+                    )) : images.map(() => (
+                        
+                        <Skeleton variant="rect" width={175} height={200}/>
                     ))}
+                </div>
+
+                <div className="button-box" id="button-box-bottom">
+
+                                {page === 0 ? 
+                                    <Button variant="contained" disabled onClick={decrementPage}>
+                                        {page === 0 ? '' : page === 1 ? page  : page}
+                                </Button> :
+                                <Button variant="contained" color="primary" onClick={decrementPage} >
+                                        {page === 0 ? '' : page === 1 ? page  : page}
+                                </Button>
+                                }
+                                
+                                <span>Page: {page + 1}/ 37000</span>
+
+                                {page >= 36999 ? 
+                                    <Button variant="contained" disabled onClick={incrementPage}>
+                                        {''}
+                                </Button> : 
+                                
+                                <Button variant="contained" color="primary" onClick={incrementPage}>
+                                        {page === 37001 ? '' : page === 37000 ? page + 1 : page + 2}
+                                </Button>
+                                }
+                
+                                </div>
+                    
                   
+                
+                
                 </div>
             )
 
