79e5dfb32317e34b00940685edcf3dc3a0120163
SJellen <sjellen@me.com>
2021-04-04 12:59:42 -0400

    split off header into a seperate file

--- src/App.js
+++ src/App.js
@@ -1,48 +1,20 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import './style/App.css';
 import Gallery from './Gallery'
 import Video from './Video'
 import Audio from './Audio'
 import Footer from './Footer'
-import useToggler from './useToggler'
-import {Context} from './Context'
+import Header from './Header'
 
-
-
-
-function App() {
-  const [show, toggle] = useToggler(true)
-
-  const {currentImage} = useContext(Context)
-
-
-  
-
-  return (
-    
-    <div className="App" >
-    <div className="header" style={{display: currentImage !== undefined ? 'none' : ''}}>
-    <h1 className="title">Harvard Art Museum Explorer</h1>
-      <i className="material-icons menu-icon" 
-             onClick={toggle}
-             style={{ display: show ? "block" : "none"}}
-             >menu_icon</i>
-        <nav style={{display: show ? "none" : "block"}}>
-          <i className="material-icons close-icon"
-              onClick={toggle}
-              style={{display: show ? "none" : "block"}}
-              >close_icon</i>
-            <a href="#home" className="firstAnchor">Image Viewer</a>
-            <a href="#video">Video Collection</a>
-            <a href="#description" className="lastAnchor">Audio Descriptions</a> 
-        </nav>
-    </div>
-      <Gallery />
-      <Video />
-      <Audio />
-      <Footer />
-    </div>
+export default function App() {
+    return (
+      <div className="App" >
+        <Header />
+        <Gallery />
+        <Video />
+        <Audio />
+        <Footer />
+      </div>
   );
 }
 
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react';
+import './style/App.css';
+import useToggler from './useToggler'
+import {Context} from './Context'
+
+
+export default function Header() {
+    const [show, toggle] = useToggler(true)
+
+    const {currentImage} = useContext(Context)
+
+    return (
+        <div className="header" style={{display: currentImage !== undefined ? 'none' : ''}}>
+            <h1 className="title">Harvard Art Museum Explorer</h1>
+            <i className="material-icons menu-icon" 
+             onClick={toggle}
+             style={{ display: show ? "block" : "none"}}
+             >menu_icon</i>
+            <nav style={{display: show ? "none" : "block"}}>
+            <i className="material-icons close-icon"
+              onClick={toggle}
+              style={{display: show ? "none" : "block"}}
+              >close_icon</i>
+            <a href="#home" className="firstAnchor">Image Viewer</a>
+            <a href="#video">Video Collection</a>
+            <a href="#description" className="lastAnchor">Audio Descriptions</a> 
+        </nav>
+    </div>
+    )
+}
